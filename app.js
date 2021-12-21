ca8a587a7a2bc7408254994f39cd2c39010b4070
terrifricker <terrifricker@yahoo.com>
2021-12-20 18:53:31 -0800

    js: stop all audio before playing new bird

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 songbirds = document.querySelector(".songbirds");
 
 songbirds.addEventListener('click', event => {
+    allSongs = document.querySelectorAll("audio");
+    allSongs.forEach(element => {
+        element.pause();
+        console.log("paused");
+    });
     console.log(event.target.parentElement.classList[1]);
     song = document.querySelector(`.${event.target.parentElement.classList[1]} audio`);
     console.log(song);
     song.play();
 })
-
-
-
-
-
-/*
-<button onclick="play()" type="button">Play </button>
-<button onclick="pause()" type="button">Pause</button>
- 
-<script>
-var myMusic= document.getElementById("music");
-function play() {
-myMusic.play();
-}
- 
-function pause() {
-myMusic.pause();
-}
-</script>
-*/
